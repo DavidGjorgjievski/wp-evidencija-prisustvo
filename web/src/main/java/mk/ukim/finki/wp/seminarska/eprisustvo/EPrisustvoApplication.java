2eb7a7c51ceb25fa97c6de5ba6ba5ba54189a9c6
DimeJovanovski <dimejovanovski@yandex.com>
2023-03-27 22:09:52 +0200

    initial commit

---
+++ web/src/main/java/mk/ukim/finki/wp/seminarska/eprisustvo/EPrisustvoApplication.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.wp.seminarska.eprisustvo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EPrisustvoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EPrisustvoApplication.class, args);
+    }
+
+}
---
+++ web/src/test/java/mk/ukim/finki/wp/seminarska/eprisustvo/EPrisustvoApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.wp.seminarska.eprisustvo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EPrisustvoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
